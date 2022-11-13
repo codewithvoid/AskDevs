aa7be663444dce803512362c38f109985589614f
Void <devwithvoid@gmail.com>
2022-11-13 10:09:42 +0530

    Add support to call users endpoint

--- src/pages/api/users/index.js
+++ src/pages/api/users/index.js
@@ -7,7 +7,7 @@ export default async function handler(req, res) {
 
   const users = files.map((file) => ({
     ...JSON.parse(fs.readFileSync(path.join(directoryPath, file), "utf8")),
-    user: file.split(".")[0],
+    username: file.split(".")[0],
   }));
 
   return res.status(200).json(users);
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,23 @@
-export default function Home() {
+export async function getServerSideProps(context) {
+  const { req } = context;
+  let users = {};
+  try {
+    const res = await fetch(`http://${req.headers.host}/api/users`);
+    users = await res.json();
+  } catch (e) {
+    console.log("Error occurred while fetching users", e);
+  }
+
+  return {
+    props: { users },
+  };
+}
+
+export default function Home({ users }) {
   return (
-    <h1 className="font-medium leading-tight text-5xl mt-0 mb-">Ask Devs</h1>
+    <div>
+      <h1 className="font-medium leading-tight text-5xl mt-0 mb-">Ask Devs</h1>
+      <code>{JSON.stringify({ res: users })}</code>
+    </div>
   );
 }
