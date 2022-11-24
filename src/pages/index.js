4a20f954a32a9511d863e3e738c1b3bcf4d56eac
Void <104728441+codewithvoid@users.noreply.github.com>
2022-11-24 12:05:29 +0000

    Filter users based on selected category

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,17 +1,19 @@
 import { TwitterShareButton } from "react-share";
+import { useState } from 'react';
+
 
 export async function getServerSideProps(context) {
   const { req } = context;
   let users, categories;
   try {
-    const usersResponse = await fetch(`http://${req.headers.host}/api/users`);
+    const usersResponse = await fetch(`http://localhost:3000/api/users`);
     users = await usersResponse.json();
     const categoriesResponse = await fetch(
-      `http://${req.headers.host}/categories.json`
+      `http://localhost:3000/categories.json`
     );
     categories = await categoriesResponse.json();
   } catch (e) {
-    console.log("Error occurred while fetching users", e);
+    console.log("Error occurred while fetching data from server", e);
   }
 
   return {
@@ -20,20 +22,28 @@ export async function getServerSideProps(context) {
 }
 
 export default function Home({ users, categories }) {
+  const [categoryUsers, setCategoryUsers] = useState(users);
+  const [selectedCategory, setSelectedCategory] = useState('');
+
+  function onCategoryButtonClick(e) {
+    const {dataset: {slug}} = e.target;
+    setSelectedCategory(slug);
+    setCategoryUsers(users.filter(u => u.categories.flatMap(c => c.slug).indexOf(slug) >= 0));
+  }
+
   function getProfileCard(user) {
     return (
       <div className="w-full p-4 md:w-1/2 lg:w-1/4" key={user.username}>
-        <div className=" bg-white flex flex-col items-center justify-center p-4 shadow-lg rounded-2xl w-64">
+        <div className="bg-white flex flex-col items-center justify-center p-4 shadow-lg rounded-2xl w-64">
           <img
             src={user.image}
             alt="profile"
             className="mx-auto rounded-full py-2 w-16 "
           />
-
-          <p className="text-gray-800 text-2xl font-medium mt-4 ">
+          <p className="font-bold text-2xl tracking-wide text-gray-800 mt-4">
             {user.name}
           </p>
-          <p className="text-gray-500 mt-2">{user.bio}</p>
+          <p className="text-gray-500 font-semibold mt-2">{user.bio}</p>
           <div className="w-full mt-8">
             <TwitterShareButton
               title={`@${user.contacts.twitter} <add your question here>`}
@@ -48,8 +58,8 @@ export default function Home({ users, categories }) {
         </div>
       </div>
     );
-  }
-
+  }  
+  
   return (
     <div>
       <header className="flex flex-wrap items-start justify-around bg-black p-3 text-gray-100">
@@ -67,16 +77,22 @@ export default function Home({ users, categories }) {
             Ask our tech twitter volunteers!
           </p>
           <div className="flex flex-wrap space-x-2">
-            {categories.map((category) => (
-              <button className="mt-1 rounded-full border-2 border-solid border-black px-5 py-1 text-center font-bold text-black hover:bg-black hover:text-white">
-                {category.name}
-              </button>
-            ))}
+            {categories.map(c =>
+              c.slug === selectedCategory ? (
+                <button className="mt-1 rounded-full border-2 border-solid border-black px-5 py-1 text-center font-bold bg-black text-white">
+                  {c.name}
+                </button>
+              ) : (
+                  <button className="mt-1 rounded-full border-2 border-solid border-black px-5 py-1 text-center font-bold text-black hover:bg-black hover:text-white" data-slug={`${c.slug}`} onClick={onCategoryButtonClick}>
+                    {c.name}
+                  </button>
+              )
+            )}
           </div>
         </section>
 
         <section className="container  mx-auto body-font h-screen min-h-screen text-gray-600 px-5 py-10">
-          <div className="-m-4 flex flex-wrap">{users.map(getProfileCard)}</div>
+          <div className="-m-4 flex flex-wrap">{categoryUsers.map(getProfileCard)}</div>
         </section>
       </main>
     </div>
