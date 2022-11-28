e80a79b56f9461c3c06015eba9e90b432405a9a0
Void <104728441+codewithvoid@users.noreply.github.com>
2022-11-28 11:04:14 +0000

    fix: setup prettier and format files

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,5 @@
-import { TwitterShareButton } from "react-share";
+import { TwitterShareButton } from 'react-share';
 import { useState } from 'react';
-
 
 export async function getServerSideProps(context) {
   const { req } = context;
@@ -8,12 +7,10 @@ export async function getServerSideProps(context) {
   try {
     const usersResponse = await fetch(`http://${req.headers.host}/api/users`);
     users = await usersResponse.json();
-    const categoriesResponse = await fetch(
-      `http://${req.headers.host}/categories.json`
-    );
+    const categoriesResponse = await fetch(`http://${req.headers.host}/categories.json`);
     categories = await categoriesResponse.json();
   } catch (e) {
-    console.log("Error occurred while fetching data from server", e);
+    console.log('Error occurred while fetching data from server', e);
   }
 
   return {
@@ -26,9 +23,11 @@ export default function Home({ users, categories }) {
   const [selectedCategory, setSelectedCategory] = useState('');
 
   function onCategoryButtonClick(e) {
-    const {dataset: {slug}} = e.target;
+    const {
+      dataset: { slug },
+    } = e.target;
     setSelectedCategory(slug);
-    setCategoryUsers(users.filter(u => u.categories.flatMap(c => c.slug).indexOf(slug) >= 0));
+    setCategoryUsers(users.filter((u) => u.categories.flatMap((c) => c.slug).indexOf(slug) >= 0));
   }
 
   function getProfileCard(user) {
@@ -39,17 +38,15 @@ export default function Home({ users, categories }) {
             src={user.image}
             alt="profile"
             className="mx-auto rounded-full py-2 w-16 "
-            style={{borderRadius:'100%'}}
+            style={{ borderRadius: '100%' }}
           />
-          <p className="font-bold text-2xl tracking-wide text-gray-800 mt-4">
-            {user.name}
-          </p>
+          <p className="font-bold text-2xl tracking-wide text-gray-800 mt-4">{user.name}</p>
           <p className="text-gray-500 font-semibold mt-2">{user.bio}</p>
           <div className="w-full mt-8">
             <TwitterShareButton
               title={`@${user.contacts.twitter} <add your question here>`}
-              url={"https://ask-devs.vercel.app/"}
-              hashtags={["AskDevs"]}
+              url={'https://ask-devs.vercel.app/'}
+              hashtags={['AskDevs']}
               className="bg-blue-400 py-2 px-4 hover:bg-blue-600 text-white w-full font-semibold rounded-lg shadow-lg"
               resetButtonStyle={false}
             >
@@ -59,8 +56,8 @@ export default function Home({ users, categories }) {
         </div>
       </div>
     );
-  }  
-  
+  }
+
   return (
     <div>
       <header className="flex flex-wrap items-start justify-around bg-black p-3 text-gray-100">
@@ -71,22 +68,22 @@ export default function Home({ users, categories }) {
 
       <main className="bg-gradient-to-r from-rose-100 to-teal-100">
         <section className="container mx-auto px-5 pt-10">
-          <h1 className="my-4 text-5xl font-bold leading-tight">
-            Got a technical question?
-          </h1>
-          <p className="mb-8 text-3xl leading-normal">
-            Ask our tech twitter volunteers!
-          </p>
+          <h1 className="my-4 text-5xl font-bold leading-tight">Got a technical question?</h1>
+          <p className="mb-8 text-3xl leading-normal">Ask our tech twitter volunteers!</p>
           <div className="flex flex-wrap space-x-2">
-            {categories.map(c =>
+            {categories.map((c) =>
               c.slug === selectedCategory ? (
                 <button className="mt-1 rounded-full border-2 border-solid border-black px-5 py-1 text-center font-bold bg-black text-white">
                   {c.name}
                 </button>
               ) : (
-                  <button className="mt-1 rounded-full border-2 border-solid border-black px-5 py-1 text-center font-bold text-black hover:bg-black hover:text-white" data-slug={`${c.slug}`} onClick={onCategoryButtonClick}>
-                    {c.name}
-                  </button>
+                <button
+                  className="mt-1 rounded-full border-2 border-solid border-black px-5 py-1 text-center font-bold text-black hover:bg-black hover:text-white"
+                  data-slug={`${c.slug}`}
+                  onClick={onCategoryButtonClick}
+                >
+                  {c.name}
+                </button>
               )
             )}
           </div>
